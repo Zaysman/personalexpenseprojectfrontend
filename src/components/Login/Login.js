5ba60d4d696d50cf97ead1a34d981fd69d597b9e
Isaiah <i.x.smith137@gmail.com>
2024-11-26 18:24:13 -0500

    initial commit

---
+++ src/components/Login/Login.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate} from 'react-router-dom';
+import './Login.css';
+
+
+function Login() {
+
+
+    return (
+    <div className = "login-container">
+
+    </div>
+    );
+}
+
+export default Login;
---
+++ src/components/SignUp/SignUp.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate} from 'react-router-dom';
+import './SignUp.css';
+
+
+function SignUp() {
+
+
+    return (
+    <div className = "signup-container">
+
+    </div>
+    );
+}
+
+export default SignUp;
